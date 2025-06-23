8b2abf495b133eebf519f3f4e7d84a5e4e6ff8d8
YumaYX <86939129+YumaYX@users.noreply.github.com>
2025-06-23 21:35:59 +0900

    first commit

---
+++ lib/ys6.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ys6/version"
+
+module Ys6
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "ys6"
+
+require "minitest/autorun"
